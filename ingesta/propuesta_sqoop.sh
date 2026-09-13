1a8d7489dfbfecd5e057e11803a47d40f39241b4
LITX05 <achavez.c05@gmail.com>
2026-09-13 20:11:34 -0300

    Cambio del ejemplo con Sqoop

--- ingesta/propuesta_sqoop.sh
+++ ingesta/propuesta_sqoop.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # Propuesta técnica de importación relacional con Apache Sqoop
 
+# Para usar sqoop se tomo el dataset y 
+# se ingreso en una base de datos de 'mySql'
+# mediante el siguente comando:
+
+# LOAD DATA INFILE '/home/cloudera/mis_datos_windows/games_march2025_cleaned.csv' 
+# INTO TABLE steam_games 
+# FIELDS TERMINATED BY ',' 
+# FIELDS TERMINATED BY ',' 
+# ENCLOSED BY '"' 
+# LINES TERMINATED BY '\n' 
+# IGNORE 1 LINES;
+
 sqoop import \
---connect jdbc:mysql://servidor/base_datos \
---username tu_usuario \
---password tu_clave \
---table clientes \
---target-dir /user/cloudera/clientes_hdfs \
--m 1
+--connect jdbc:mysql://127.0.0.1/proyecto_I \ --LOCALHOST--
+--username root \
+--password cloudera \
+--table steam_games \
+--m 1 \
+--target-dir /user/cloudera/steam_games_hdfs
